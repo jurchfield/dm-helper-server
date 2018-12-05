b97317cb0332799664338a3925d5bf65a2832f92
John Burchfield <jurchfield@gmail.com>
2018-12-04 19:15:45 -0600

    Test

--- functions/weapons.js
+++ functions/weapons.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { firestore } = require('firebase-functions-helper');
 
 function modifyValue(val) {
   const url = val.replace('http://www.dnd5eapi.co/api/equipment/', '')
@@ -8,7 +9,18 @@ function modifyValue(val) {
   return `https://us-central1-dm-helper-1f262.cloudfunctions.net/weapons?id=${id}`;
 }
 
-exports.handler = (req, res) => {
+function POST({ body }, res, db) {
+  JSON.parse(body).forEach(element => {
+    firestore.createNewDocument(db, 'equipment', element);
+  });
+
+  return res.status(200).send( { message: `${JSON.parse(body).length} records added` } );
+}
+
+exports.handler = (req, res, db) => {
+  
+  if (req.method === 'POST') return POST(req, res, db);
+  
   if (req.method !== 'GET') return res.status(403);
 
   if (req.query.id) {
